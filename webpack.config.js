ac2ce285643cb2f3521945b945bfc959551d5fed
sangay wangchuk <sangaywangchuk100@gmail.com>
2024-02-13 15:27:48 +0600

    webpack dist setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx')
+  },
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [new CopyPlugin({
+    patterns: [
+      {
+        from: path.resolve('src/manifest.json'),
+        to: path.resolve('dist')
+      }
+    ]
+  }),
+  ...getHtmlPlugins(['popup', 'options'])
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(chunk => new HtmlPlugin({
+    title: "React Extension",
+    filename: `${chunk}.html`,
+    chunks: [chunk]
+  }))
+}
