2df54b6b27eb3ab8a590a320c796ef166877aa10
sangay wangchuk <sangaywangchuk100@gmail.com>
2024-02-13 18:18:32 +0600

    prod build setup webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
-    options: path.resolve('src/options/options.tsx')
+    options: path.resolve('src/options/options.tsx'),
+    background: path.resolve('src/background/background.ts'),
+    contentScript: path.resolve('src/contentScript/contentScript.ts')
   },
   module: {
     rules: [
@@ -26,15 +27,19 @@ module.exports = {
       }
     ]
   },
-  plugins: [new CopyPlugin({
-    patterns: [
-      {
-        from: path.resolve('src/public'),
-        to: path.resolve('dist')
-      }
-    ]
-  }),
-  ...getHtmlPlugins(['popup', 'options'])
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve('src/public'),
+          to: path.resolve('dist')
+        }
+      ]
+    }),
+    ...getHtmlPlugins(['popup', 'options'])
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
